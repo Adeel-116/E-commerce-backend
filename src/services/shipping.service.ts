0eae1ad438114f9e1d4486b0c957bb1fb4c3e2fc
Adeel-116 <adeel8128377@gmail.com>
2026-07-25 14:17:59 +0500

    Adeel feat ( fix the deployment issue )

--- src/services/shipping.service.ts
+++ src/services/shipping.service.ts
@@ -1,5 +1,5 @@
-export const SHIPPING_COST = 20;
-export const FREE_SHIPPING_THRESHOLD = 200;
+export const SHIPPING_COST = 200;
+export const FREE_SHIPPING_THRESHOLD = 3000;
 const STANDARD_SHIPPING_LABEL = "Standard shipping";
 const FREE_SHIPPING_LABEL = "Free shipping";
 
